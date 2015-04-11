e2d43f95baa94454972e9e5554b531c415009db0
Cheboksarinov <cheboksarinov.andrey@gmail.com>
2015-04-11 19:00:01 +0300

    Я могу нарисовать стенку нажатием левой кнопки

--- src/ru/nnsu/pacman/common/Map.java
+++ src/ru/nnsu/pacman/common/Map.java
@@ -4,4 +4,24 @@ import java.io.Serializable;
 
 public class Map implements Serializable {
 
+    private final int mapGrid[][];
+    
+    public Map() {
+        mapGrid = new int[getWeight()][getHeight()];
+    }
+    
+    public final int getWeight(){
+        return 10;
+    }
+    
+    public final int getHeight(){
+        return 10;
+    }
+
+    public void SetCellAsWall(int x, int y) {
+        mapGrid[x][y] = 1;
+    }
+    public int GetCellValue(int x,int y){
+        return mapGrid[x][y];
+    }
 }
