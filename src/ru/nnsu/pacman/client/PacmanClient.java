c4a3d44602525c788056e9c268f5a183c8bbc5f0
Oleg Blokhin <oleg.blohin@smartstepgroup.com>
2015-04-11 14:25:03 +0300

    [1] При запуске игры я вижу форму подключения к серверу

--- src/ru/nnsu/pacman/client/PacmanClient.java
+++ src/ru/nnsu/pacman/client/PacmanClient.java
@@ -1,7 +1,34 @@
 package ru.nnsu.pacman.client;
 
-public class PacmanClient {
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class PacmanClient extends JFrame  {
+    public PacmanClient() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        add(new StartForm());
+
+        setSize(350, 200);
+
+        setTitle("PacmanGame");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    } 
+    
     public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                PacmanClient ex = new PacmanClient();
+                ex.setVisible(true);
+            }
+        });
     }
     
 }
