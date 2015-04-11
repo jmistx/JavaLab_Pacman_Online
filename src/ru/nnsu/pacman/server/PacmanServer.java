39d7bb145b7accec6138f3a2f1c5a1a1f5c0ec4a
Oleg Blokhin <oleg.blohin@smartstepgroup.com>
2015-04-11 14:35:41 +0300

    [2] При запуске сервера я вижу настройки

--- src/ru/nnsu/pacman/server/PacmanServer.java
+++ src/ru/nnsu/pacman/server/PacmanServer.java
@@ -1,6 +1,30 @@
 package ru.nnsu.pacman.server;
 
-public class PacmanServer {
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class PacmanServer  extends JFrame  {
+    public PacmanServer() {
+        initUI();
+    }
+
+    private void initUI() {
+        add(new ServerStartForm());
+
+        setSize(350, 200);
+
+        setTitle("Pacman Server");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    } 
+    
     public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                PacmanServer ex = new PacmanServer();
+                ex.setVisible(true);
+            }
+        });
     }
 }
